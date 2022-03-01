73bbfe0ba37f6e162ae67496e53c3131948a6bad
alberto-formacion <acortazar83@gmail.com>
2022-03-01 17:54:57 +0100

    Add files via upload

---
+++ repaso/src/repaso/Main.java
@@ -0,0 +1,18 @@
+package repaso;
+
+public class Main {
+
+    public static void main(String[] args) {
+             /**
+             * nombre/tipoAnimal/raza/color/nombredueño/fechaNacimineto
+             * "Pulgas"/"perro"/"bulldog"/"canela"/"Pepe"/"01/04/2020"
+             * 1.-Crear animal
+             * 2.-Ver animal
+             * 3.-Editar animal
+             * 4.-Borrar animal
+              * 5.-Salir  
+              */
+
+    }
+
+}
---
+++ repasoB/src/repasoB/Main.java
@@ -0,0 +1,47 @@
+package repasoB;
+
+public class Main {
+
+    //crear una variable global llamada animal de tipo array de String de seis posiciones
+
+    public static void main(String[] args) {
+             //crear una variable opcion de tipo entero
+             //crear un do...while(mientras que opcion sea distinto de 5)
+                       //pintar el menu
+
+                       /**
+
+                       * 1.-Crear animal
+
+                       * 2.-Ver animal
+
+                       * 3.-Editar animal
+
+                       * 4.-Borrar animal
+
+                       * 5.-Salir
+
+                       */
+
+                       //pedir al usuario que introduzca una opcion y guardarla en la variable opcion
+                       //hacer un switch con la opcion
+                                //en caso que sea 1
+                                          /*
+
+                                          * insertar los datos del animal
+
+                                          * pedir los datos uno a uno de... nombre/tipoAnimal/raza/color/nombredueño/fechaNacimineto
+
+                                          * "Pulgas"/"perro"/"bulldog"/"canela"/"Pepe"/"01/04/2020"
+
+                                          */
+                                //en caso que sea 2
+                                          //mostrar por consola los datos del array utilizando un for
+                                //en caso que sea 3
+                                          //pedir los datos uno a uno de... nombre/tipoAnimal/raza/color/nombredueño/fechaNacimineto y cambiar los valores del array
+                                //en caso que sea 4
+                                          //poner a null todos los valores del array utilizando un for
+
+    }
+
+}
