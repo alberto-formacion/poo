8930fa0b9036214e1f7248c432f69d2bc8832b5a
alberto-formacion <acortazar83@gmail.com>
2022-04-26 18:35:30 +0200

    Add files via upload

---
+++ ventanas/src/ventanas/VentanaLista.java
@@ -0,0 +1,111 @@
+package ventanas;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+
+public class VentanaLista {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaLista window = new VentanaLista();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaLista() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JList<String> listLeguajes = new JList<String>();
+		listLeguajes.setBounds(37, 35, 107, 181);
+		
+		DefaultListModel<String> modelLenguajes = new DefaultListModel<String>();
+		modelLenguajes.addElement("Java");
+		modelLenguajes.addElement("C#");
+		modelLenguajes.addElement(".Net");
+		modelLenguajes.addElement("PHP");
+		
+		listLeguajes.setModel(modelLenguajes);
+		
+		frame.getContentPane().add(listLeguajes);
+		
+		JList<String> listCompetencias = new JList<String>();
+		
+		DefaultListModel<String> modelCompetencias = new DefaultListModel<String>();
+		
+		listCompetencias.setModel(modelCompetencias);
+
+		listCompetencias.setBounds(253, 35, 107, 181);
+		frame.getContentPane().add(listCompetencias);
+		
+		JButton btnPasarDrchUno = new JButton(">");
+		btnPasarDrchUno.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int posicion = listLeguajes.getSelectedIndex();
+				String lenguaje = listLeguajes.getModel().getElementAt(posicion);
+				
+				DefaultListModel<String> modeloC = (DefaultListModel<String>) listCompetencias.getModel();
+				modeloC.addElement(lenguaje);
+				
+				DefaultListModel<String> modelL = (DefaultListModel<String>) listLeguajes.getModel();
+				modelL.removeElement(lenguaje);
+				
+			}
+		});
+		btnPasarDrchUno.setBounds(154, 64, 89, 23);
+		frame.getContentPane().add(btnPasarDrchUno);
+		
+		JButton btnPasarDrchTodos = new JButton(">>");
+		btnPasarDrchTodos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DefaultListModel<String> modelL = (DefaultListModel<String>) listLeguajes.getModel();
+				DefaultListModel<String> modelC = (DefaultListModel<String>) listCompetencias.getModel();
+				
+				for(int i=0;i<modelL.getSize();i++) {
+					modelC.addElement(modelL.getElementAt(i));
+				}
+				
+				modelL.removeAllElements();
+			}
+		});
+		btnPasarDrchTodos.setBounds(154, 98, 89, 23);
+		frame.getContentPane().add(btnPasarDrchTodos);
+		
+		JButton btnPasarIzqUno = new JButton("<");
+		btnPasarIzqUno.setBounds(154, 132, 89, 23);
+		frame.getContentPane().add(btnPasarIzqUno);
+		
+		JButton btnPasarIzqTodos = new JButton("<<");
+		btnPasarIzqTodos.setBounds(154, 166, 89, 23);
+		frame.getContentPane().add(btnPasarIzqTodos);
+	}
+}
---
+++ ventanas/src/ventanas/VentanaRadioCheck.java
@@ -0,0 +1,43 @@
+package ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class VentanaRadioCheck {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaRadioCheck window = new VentanaRadioCheck();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaRadioCheck() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
